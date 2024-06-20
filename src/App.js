7a8908524f2fd18719a391f869c73879818e7b96
lenaozdemir <lenaozdemirs1@gmail.com>
2024-06-20 13:39:57 +0300

    add hw week 23

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Tariff from './Tarif';
+
 
 function App() {
+  const tarifs = [
+    {name: "Безлимит 300", price: '300', speed: 'До 10 Мбит/сек', background: 'blue'},
+    {name: "Безлимит 450", price: '450', speed: 'До 50 Мбит/сек', background: 'green'},
+    {name: "Безлимит 500", price: '500', speed: 'До 100 Мбит/сек', background: 'red'},
+    {name: "Безлимит 1000", price: '1000', speed: 'До 200 Мбит/сек', background: 'black'}
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tarifs'>
+      {tarifs.map((tarif, index) => (
+        <Tariff 
+        key={index}
+        name={tarif.name}
+        price={tarif.price}
+        speed={tarif.speed}
+        background={tarif.background}
+        highlight={tarif.price === '500'}
+        />
+      ))}
     </div>
   );
 }
